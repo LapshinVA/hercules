a284dabd64eb130178fe096c9f6baa7c2a5b80c3
Vadim Lapshin <lapshin_va@list.ru>
2024-03-09 12:44:51 +0300

    init

---
+++ src/test/java/ru/project/pet/hermes/HermesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.pet.hermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
